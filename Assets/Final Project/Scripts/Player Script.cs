4c356a1a84dae37542db917c70cf9ff868cb9147
ECCNYGame <ea456bx@bhsec.bard.edu>
2025-11-22 11:57:00 -0500

    s

--- Assets/Final Project/Scripts/Player Script.cs
+++ Assets/Final Project/Scripts/Player Script.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerScript : MonoBehaviour
+public class GeoController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string nextLevel = "lvl.2";
+    private Rigidbody2D _rigidbody2D;
+    public int speed = 5;
+    private SpriteRenderer _spriteRenderer;
+
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
+    { //Movement
+        float xInput = Input.GetAxis("Horizontal");
+        _rigidbody2D.velocity = new Vector2(xInput * speed, _rigidbody2D.velocity.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            _spriteRenderer.color = Color.red;
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            _spriteRenderer.color = Color.blue;
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            _spriteRenderer.color = Color.green;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        switch (collision.tag)
+        {
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    break;
+                }
+        }
     }
 }
